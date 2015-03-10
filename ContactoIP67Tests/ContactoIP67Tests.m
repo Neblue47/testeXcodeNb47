a6778eaf8d8894223605778c858528e03b8ee9ef
ios5034 <ios5034@Mac-mini-de-caelum-2.local>
2015-03-10 18:57:19 -0300

    Initial Commit

---
+++ ContactoIP67Tests/ContactoIP67Tests.m
@@ -0,0 +1,40 @@
+//
+//  ContactoIP67Tests.m
+//  ContactoIP67Tests
+//
+//  Created by ios5034 on 10/03/15.
+//  Copyright (c) 2015 ios5034. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface ContactoIP67Tests : XCTestCase
+
+@end
+
+@implementation ContactoIP67Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
